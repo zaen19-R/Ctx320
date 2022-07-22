dbe5cc929ef3b0f1654971658f0a03f27e9200f3
zaen19-R <sangpnikmat@gmail.com>
2022-07-22 14:29:40 +0700

    fix 10

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -16,17 +16,17 @@ const Main = ({ children, router }) => {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Rozandi Hikmah homepage" />
-        <meta name="author" content="Takuya Matsuyama" />
+        <meta name="author" content="Rozandi Hikmah" />
         <meta name="author" content="craftzdog" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <meta name="twitter:title" content="Takuya Matsuyama" />
+        <meta name="twitter:title" content="Rozandi Hikmah" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@craftzdog" />
         <meta name="twitter:creator" content="@craftzdog" />
         <meta name="twitter:image" content="https://www.craftz.dog/card.png" />
-        <meta property="og:site_name" content="Takuya Matsuyama" />
-        <meta name="og:title" content="Takuya Matsuyama" />
+        <meta property="og:site_name" content="Rozandi Hikmah" />
+        <meta name="og:title" content="Rozandi Hikmah" />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="https://www.craftz.dog/card.png" />
         <title>Ctx320 - Homepage</title>
