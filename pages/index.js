82420f78a15786377eccbe796bf7e5bbb9fa0e49
zaen19-R <sangpnikmat@gmail.com>
2022-07-22 12:26:39 +0700

    fix 2

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ import {
   Container,
   Heading,
   Box, 
-  SimpleGrid,
   Button,
   List,
   ListItem,
@@ -16,9 +15,7 @@ import Paragraph from '../components/paragraph'
 import { BioSection, BioYear } from '../components/bio'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
-import { IoLogoTwitter, IoLogoInstagram, IoLogoGithub } from 'react-icons/io5'
-import thumbYouTube from '../public/images/links/youtube.png'
-import thumbInkdrop from '../public/images/works/inkdrop_eyecatch.png'
+import { IoLogoInstagram } from 'react-icons/io5'
 import Image from 'next/image'
 
 const ProfileImage = chakra(Image, {
