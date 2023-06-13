6cf5489cf76456399a323fc9f461cce53be35512
Rozandi Hikmah <43483942+zaen19-R@users.noreply.github.com>
2023-06-13 11:52:25 +0700

    popup

---
+++ components/Popup.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react'
+import { Image } from '@chakra-ui/react'
+import { Heading, Box, Button, Grid } from '@chakra-ui/react'
+import { Modal, ModalOverlay, ModalContent } from '@chakra-ui/react'
+
+const AutoLoadPopup = () => {
+  const [isOpen, setIsOpen] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsOpen(true)
+    }, 15)
+
+    return () => clearTimeout(timer)
+  }, [])
+
+  const handleClose = () => {
+    setIsOpen(false)
+  }
+
+  return (
+    <Modal isOpen={isOpen}>
+      <ModalOverlay />
+      <ModalContent padding="10px" bg="bl" backdropFilter="blur(8px)">
+        <Box display={'flex'} justifyContent={'flex-end'}>
+          <Button onClick={handleClose} colorScheme="teal">
+            Close
+          </Button>
+        </Box>
+        <Box mt={-5} height={'150'}>
+          <Grid templateColumns="auto 1fr" columnGap={4} alignItems="center">
+            <Image
+              src="/images/zandybanner.png"
+              alt="Zandy Banner"
+              height={150}
+              rounded={'100%'}
+            />
+            <Box>
+              <Heading as="h1">Welcome</Heading>
+              <Heading as="h4" variant="body-paraf">
+                to Ctx320
+              </Heading>
+            </Box>
+          </Grid>
+        </Box>
+      </ModalContent>
+    </Modal>
+  )
+}
+
+export default AutoLoadPopup
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import {
   Link,
   Container,
   Heading,
-  Box, 
+  Box,
   Button,
   List,
   ListItem,
@@ -15,170 +15,208 @@ import Paragraph from '../components/paragraph'
 import { BioSection, BioYear } from '../components/bio'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
-import { IoLogoGithub, IoLogoInstagram, IoLogoLinkedin } from 'react-icons/io5'
+import {
+  IoLogoGithub,
+  IoLogoInstagram,
+  IoLogoLinkedin,
+  IoLogoYoutube
+} from 'react-icons/io5'
 import Image from 'next/image'
+import { useState } from 'react'
+import Popup from '../components/Popup.js'
 
 const ProfileImage = chakra(Image, {
   shouldForwardProp: prop => ['width', 'height', 'src', 'alt'].includes(prop)
 })
 
-const Home = () => (
-  <Layout>
-    <Container>
-      <Box
-        borderRadius="lg"
-        mb={6}
-        p={3}
-        textAlign="center"
-        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
-        css={{ backdropFilter: 'blur(10px)' }}
-      >
-        Hello, I&apos;m an indie app developer based in Indonesia!
-      </Box>
+const Home = () => {
+  const [isPopupOpen, setIsPopupOpen] = useState(false)
 
-      <Box display={{ md: 'flex' }}>
-        <Box flexGrow={1}>
-          <Heading as="h2" variant="page-title">
-            Rozandi Hikmah
-          </Heading>
-          <p>Digital Craftsman ( Artist / Developer / Designer )</p>
-        </Box>
+  const handleOpenPopup = () => {
+    setIsPopupOpen(true)
+  }
+
+  const handleClosePopup = () => {
+    setIsPopupOpen(false)
+  }
+
+  return (
+    <Layout>
+      <Container>
+        <Popup isOpen={isPopupOpen} onClose={handleClosePopup} />
         <Box
-          flexShrink={0}
-          mt={{ base: 4, md: 0 }}
-          ml={{ md: 6 }}
+          borderRadius="lg"
+          mb={6}
+          p={3}
           textAlign="center"
+          bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+          css={{ backdropFilter: 'blur(10px)' }}
         >
+          Hello, I&apos;m an indie app developer based in Indonesia!
+        </Box>
+
+        <Box display={{ md: 'flex' }}>
+          <Box flexGrow={1}>
+            <Heading as="h2" variant="page-title">
+              Rozandi Hikmah
+            </Heading>
+            <p>Digital Craftsman ( Artist / Developer / Designer )</p>
+          </Box>
           <Box
-            borderColor="whiteAlpha.800"
-            borderWidth={2}
-            borderStyle="solid"
-            w="100px"
-            h="100px"
-            display="inline-block"
-            borderRadius="full"
-            overflow="hidden"
+            flexShrink={0}
+            mt={{ base: 4, md: 0 }}
+            ml={{ md: 6 }}
+            textAlign="center"
           >
-            <ProfileImage
-              src="/images/zandy.jpg"
-              alt="Profile image"
+            <Box
+              borderColor="whiteAlpha.800"
+              borderWidth={2}
+              borderStyle="solid"
+              w="100px"
+              h="100px"
+              display="inline-block"
               borderRadius="full"
-              width="100%"
-              height="100%"
-            />
+              overflow="hidden"
+            >
+              <ProfileImage
+                src="/images/zandy.jpg"
+                alt="Profile image"
+                borderRadius="full"
+                width="100%"
+                height="100%"
+              />
+            </Box>
           </Box>
         </Box>
-      </Box>
 
-      <Section delay={0.1}>
-        <Heading as="h3" variant="section-title">
-          Work
-        </Heading>
-        <Paragraph>
-          Rozandi Hikmah is a freelance and a full-stack developer based in Palembang with a
-          passion for building digital services/stuff. He has a knack
-          for all things launching products, from planning and designing all the
-          way to solving real-life problems with code. When not online, he loves
-          hanging out with his camera.
-        </Paragraph>
-        <Box align="center" my={4}>
-          <NextLink href="/works" passHref scroll={false}>
-            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
-              My portfolio
-            </Button>
-          </NextLink>
-        </Box>
-      </Section>
-
-      <Section delay={0.2}>
-        <Heading as="h3" variant="section-title" mt={5}>
-          Bio
-        </Heading>
-        <BioSection>
-          <BioYear>1997</BioYear>
-          Born in Palembang, Indonesia.
-        </BioSection>
-        <BioSection>
-          <BioYear>2015 to 2019</BioYear>
-          Tax Staff of PT Adelindo at Palembang 
-        </BioSection>
-        <BioSection>
-          <BioYear>2019 to 2021</BioYear>
-          IT Support of PT Hero Supermarket Tbk at Palembang 
-        </BioSection>
-        <BioSection>
-          <BioYear>2021 to 2022</BioYear>
-          Sales Executive of PT Home Center Indonesia at Palembang 
-        </BioSection>
-        <BioSection>
-          <BioYear>2022 to present</BioYear>
-          Assisten Supervisor of MR DIY Indonesia
-        </BioSection>
-      </Section>
-
-      <Section delay={0.3}>
-        <Heading as="h3" variant="section-title">
-          I ♥
-        </Heading>
-        <Paragraph>
-          Art, Music,{' '}
-          <Link href="https://www.instagram.com/tama_z30" target="_blank" textColor={'white'}>
-            Drawing
-          </Link>
-          ,Photography
-          , Machine Learning
-        </Paragraph>
-      </Section>
-
-      <Section delay={0.3}>
-        <Heading as="h3" variant="section-title">
-          On the web
-        </Heading>
-        <List>
-          <ListItem>
-            <Link href="https://www.instagram.com/tama_z30" target="_blank">
-              <Button
-                variant="ghost"
-                colorScheme="teal"
-                leftIcon={<IoLogoInstagram />}
-              >
-                @tama_z30
+        <Section delay={0.1}>
+          <Heading as="h3" variant="section-title">
+            Work
+          </Heading>
+          <Paragraph>
+            Rozandi Hikmah is a freelance and a full-stack developer based in
+            Palembang with a passion for building digital services/stuff. He has
+            a knack for all things launching products, from planning and
+            designing all the way to solving real-life problems with code. When
+            not online, he loves hanging out with his camera.
+          </Paragraph>
+          <Box align="center" my={4}>
+            <NextLink href="/works" passHref scroll={false}>
+              <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+                My portfolio
               </Button>
+            </NextLink>
+          </Box>
+        </Section>
+
+        <Section delay={0.2}>
+          <Heading as="h3" variant="section-title" mt={5}>
+            Bio
+          </Heading>
+          <BioSection>
+            <BioYear>1997</BioYear>
+            Born in Palembang, Indonesia.
+          </BioSection>
+          <BioSection>
+            <BioYear>2015 to 2019</BioYear>
+            Tax Staff of PT Adelindo at Palembang
+          </BioSection>
+          <BioSection>
+            <BioYear>2019 to 2021</BioYear>
+            IT Support of PT Hero Supermarket Tbk at Palembang
+          </BioSection>
+          <BioSection>
+            <BioYear>2021 to 2022</BioYear>
+            Sales Executive of PT Home Center Indonesia at Palembang
+          </BioSection>
+          <BioSection>
+            <BioYear>2022 to present</BioYear>
+            Assisten Supervisor of MR DIY Indonesia
+          </BioSection>
+        </Section>
+
+        <Section delay={0.3}>
+          <Heading as="h3" variant="section-title">
+            I ♥
+          </Heading>
+          <Paragraph>
+            Art, Music,{' '}
+            <Link
+              href="https://www.instagram.com/tama_z30"
+              target="_blank"
+              variant="baseStyle"
+            >
+              Drawing
             </Link>
-          </ListItem>
-          <ListItem>
-            <Link href="https://github.com/zaen19-R" target="_blank">
-              <Button
-                variant="ghost"
-                colorScheme="teal"
-                leftIcon={<IoLogoGithub />}
+            , Photography , Machine Learning
+          </Paragraph>
+        </Section>
+
+        <Section delay={0.3}>
+          <Heading as="h3" variant="section-title">
+            On the web
+          </Heading>
+          <List>
+            <ListItem>
+              <Link href="https://www.instagram.com/tama_z30" target="_blank">
+                <Button
+                  variant="ghost"
+                  colorScheme="teal"
+                  leftIcon={<IoLogoInstagram />}
+                >
+                  @tama_z30
+                </Button>
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link href="https://github.com/zaen19-R" target="_blank">
+                <Button
+                  variant="ghost"
+                  colorScheme="teal"
+                  leftIcon={<IoLogoGithub />}
+                >
+                  Rozandi Hikmah
+                </Button>
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link
+                href="https://www.linkedin.com/in/rozandi-hikmah/"
+                target="_blank"
               >
-                Rozandi Hikmah
+                <Button
+                  variant="ghost"
+                  colorScheme="teal"
+                  leftIcon={<IoLogoLinkedin />}
+                >
+                  Rozandi Hikmah
+                </Button>
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link href="https://www.youtube.com/@zandizoldyc" target="_blank">
+                <Button
+                  variant="ghost"
+                  colorScheme="teal"
+                  leftIcon={<IoLogoYoutube />}
+                >
+                  Zandi Zoldyc
+                </Button>
+              </Link>
+            </ListItem>
+          </List>
+          <Box align="center" my={4}>
+            <NextLink href="/posts" passHref scroll={false}>
+              <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+                Popular posts
               </Button>
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link href='https://www.linkedin.com/in/rozandi-hikmah/' target='_blank'>
-            <Button
-            variant='ghost'
-            colorScheme='teal'
-            leftIcon={<IoLogoLinkedin/>}>
-              Rozandi Hikmah
-            </Button>
-            </Link>
-          </ListItem>
-        </List>
-        <Box align="center" my={4}>
-          <NextLink href="/posts" passHref scroll={false}>
-            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
-              Popular posts
-            </Button>
-          </NextLink>
-        </Box>
-      </Section>
-    </Container>
-  </Layout>
-)
+            </NextLink>
+          </Box>
+        </Section>
+      </Container>
+    </Layout>
+  )
+}
 
 export default Home
 export { getServerSideProps } from '../components/chakra'
