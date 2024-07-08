e0729285a6f80fba0d9a9fad892a1b821806d77a
Rozandi Hikmah <sangpnikmat@gmail.com>
2024-07-08 12:50:16 +0700

    test2fix01

--- pages/api/tampil.js
+++ pages/api/tampil.js
@@ -1,13 +1,15 @@
 import { promises as fs } from 'fs'
 
-export default async function Page() {
-  const file = await fs.readFile(process.cwd() + 'pages/api/data.json', 'utf8')
-  const data = JSON.parse(file)
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    const file = await fs.readFile(
+      process.cwd() + 'pages/api/data.json',
+      'utf8'
+    )
+    const data = JSON.parse(file)
 
-  return (
-    <div>
-      <h1>{data.title}</h1>
-      <p>{data.content}</p>
-    </div>
-  )
+    res.status(200).JSON(data)
+  } else {
+    res.status(404).send('Not Found')
+  }
 }
