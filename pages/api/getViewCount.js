e8282dbf0f44c1ae3f588889808ae93d188b32e8
Rozandi Hikmah <sangpnikmat@gmail.com>
2024-07-08 12:40:25 +0700

    test2

--- pages/api/getViewCount.js
+++ pages/api/getViewCount.js
@@ -6,9 +6,10 @@ export default async function handler(req, res) {
   try {
     // const filePath = '../pages/api/test.json'
     // const filePath = 'data/test.json'
-    const filePath = 'pages/api/test.json'
+    // const filePath = 'pages/api/test.json'
+    const filePath = '/api/test.json'
     // const filePath = './pages/api/counter.json'
-    const permissions = 0o600
+    // const permissions = 0o600
 
     // Set the file permissions to read and write (owner only)
     await chmod(filePath, permissions)
@@ -21,8 +22,8 @@ export default async function handler(req, res) {
     // count++
 
     // Save the updated visitor count to the data file
-    await writeFile(filePath, JSON.stringify({ count }), 'utf-8')
-    count++
+    // await writeFile(filePath, JSON.stringify({ count }), 'utf-8')
+    // count++
     // Return the updated visitor count as the response
     res.status(200).json({ visitorCount: count })
   } catch (error) {
